0557da46ad23b1f6363d57ba35712024d6dacbc0
Agustinus Nathaniel <berserk.nate@gmail.com>
2026-04-27 08:50:32 +0700

    refactor: add umami track

--- apps/toolbox-web/env.ts
+++ apps/toolbox-web/env.ts
@@ -8,5 +8,7 @@ export default defineConfig({
     VITE_API_BASE_URL: z.string().optional(),
     ENABLE_PLUGIN_REACT_COMPILER: z.stringbool().optional(),
     VITE_ANALYTICS_DEBUG: z.stringbool().optional(),
+    VITE_UMAMI_SCRIPT_URL: z.string().optional(),
+    VITE_UMAMI_WEBSITE_ID: z.string().optional(),
   },
 });
