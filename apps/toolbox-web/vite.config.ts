4f019bf2cac14760224811ad8218d1b211d225d8
Agustinus Nathaniel <berserk.nate@gmail.com>
2026-04-27 08:55:34 +0700

    chore: disable pwa - temp

--- apps/toolbox-web/vite.config.ts
+++ apps/toolbox-web/vite.config.ts
@@ -9,7 +9,7 @@ import { VitePWA, type VitePWAOptions } from 'vite-plugin-pwa';
 import { defineConfig, loadEnv } from 'vite-plus';
 
 const pwaOptions: Partial<VitePWAOptions> = {
-  // disable: true,
+  disable: true,
   registerType: 'autoUpdate',
   base: '/',
   manifest: {
